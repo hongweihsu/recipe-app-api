34f82e39e471df1c0af6ffeee24c2eee0300f081
hongweihsu <hongweih@student.unimelb.edu.au>
2022-10-01 00:23:26 +1000

    add user app

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),  # generate yml for api
+    # use yml file to serve swagger view
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
+
 ]

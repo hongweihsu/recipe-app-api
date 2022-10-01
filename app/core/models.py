a428946a8b1adea39190212e6b028c2cf0323442
hongweihsu <hongweih@student.unimelb.edu.au>
2022-10-01 11:59:12 +1000

    add user app

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 """database models"""
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
@@ -36,3 +37,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # e.g core.User, referencing from setting is best practice
+        on_delete=models.CASCADE,  # once user is deleted, his recipes also deleted
+    )
+    title = models.CharField(max_length=255)  # more efficient than TextField
+    description = models.TextField(blank=True)  # more content than CharField
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):  # if print a obj in str(), it will show ... (self.title in this case)
+        return self.title
